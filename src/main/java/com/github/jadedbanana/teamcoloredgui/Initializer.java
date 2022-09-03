0d5d18e4b49942e38b76456d8ea07618da748d00
popki <popkilj289@gmail.com>
2022-09-03 18:17:24 -0400

    Removed unused imports, renamed ExampleMod to Initializer

--- src/main/java/com/github/jadedbanana/teamcoloredgui/Initializer.java
+++ src/main/java/com/github/jadedbanana/teamcoloredgui/Initializer.java
@@ -3,19 +3,15 @@ package com.github.jadedbanana.teamcoloredgui;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-public class ExampleMod implements ModInitializer {
+	
+public class Initializer implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("teamcoloredgui");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("[Team Colored GUI] Initialized.");
 	}
 }
--- src/main/java/com/github/jadedbanana/teamcoloredgui/mixin/InGameHudMixin.java
+++ src/main/java/com/github/jadedbanana/teamcoloredgui/mixin/InGameHudMixin.java
@@ -1,13 +1,11 @@
 package com.github.jadedbanana.teamcoloredgui.mixin;
 
-import com.github.jadedbanana.teamcoloredgui.ExampleMod;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
