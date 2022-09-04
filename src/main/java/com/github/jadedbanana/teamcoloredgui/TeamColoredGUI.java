b757bdb0ac90364324bff0428eb5a6b30dc56f14
popki <popkilj289@gmail.com>
2022-09-04 08:38:31 -0400

    Added keybinds

--- src/main/java/com/github/jadedbanana/teamcoloredgui/TeamColoredGUI.java
+++ src/main/java/com/github/jadedbanana/teamcoloredgui/TeamColoredGUI.java
@@ -1,13 +1,14 @@
 package com.github.jadedbanana.teamcoloredgui;
 
+import com.github.jadedbanana.teamcoloredgui.keybinds.KeyBinds;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Initializer implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+public class TeamColoredGUI implements ModInitializer {
+
+	private KeyBinds keyBinds;
 	public static final Logger LOGGER = LoggerFactory.getLogger("teamcoloredgui");
 
 	@Override
@@ -15,6 +16,10 @@ public class Initializer implements ModInitializer {
 		// Initialize team texture finder.
 		LOGGER.info("[Team Colored GUI] Loading team colors...");
 		TeamTextureFinder.initialize();
+
+		// Initialize keybinds.
+		LOGGER.info("[Team Colored GUI] Loading keybinds...");
+		this.keyBinds = new KeyBinds();
 
 		// Loaded.
 		LOGGER.info("[Team Colored GUI] Loaded.");
---
+++ src/main/java/com/github/jadedbanana/teamcoloredgui/keybinds/KeyBinds.java
@@ -0,0 +1,39 @@
+package com.github.jadedbanana.teamcoloredgui.keybinds;
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+
+public class KeyBinds
+{
+    private KeyBinding[] keyBinding;
+
+
+    /*
+    Constructor.
+     */
+    public KeyBinds() {
+        this.keyBinding = new KeyBinding[2];
+        this.setKeybinds();
+    }
+
+
+    /*
+    Create keybind objects.
+    Openconfig is set to page up by default.
+    Toggle is not bound by default.
+     */
+    public void setKeybinds() {
+        this.keyBinding[0] = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.teamcoloredgui.openconfig",
+                266,
+                "key.teamcoloredgui.category"
+        ));
+        this.keyBinding[1] = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.teamcoloredgui.toggle",
+                -1,
+                "key.teamcoloredgui.category"
+        ));
+    }
+
+}
