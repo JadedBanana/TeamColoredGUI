d7cbfaee9d48275cca01546595769d80f55603e3
popki <popkilj289@gmail.com>
2022-10-01 13:38:53 -0400

    Revert "renamed image_color_replace_by_value.py to image_color_replace_by_value_linear.py"
    This reverts commit 6bce8d95ed54c4d90eeabe7aaaaf509b818e0e08.

--- image_gen/gen_tipped_arrow.sh
+++ image_gen/gen_tipped_arrow.sh
@@ -18,96 +18,96 @@ TIP_TINT_STRENGTH=0.5
 
 # black
 echo -e "Generating BLACK tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(12,12,12)" "(5,5,5)" 1 0
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(12,12,12)" "(5,5,5)" 1 0
 python3 image_color_multiply.py tipped_arrow/tip.png "(12,12,12)" $TIP_TINT_STRENGTH
 merge_images black
 
 # dark blue
 echo -e "Generating DARK BLUE tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(0,0,168)" "(0,0,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(0,0,168)" "(0,0,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(0,0,168)" $TIP_TINT_STRENGTH
 merge_images dark_blue
 
 # dark green
 echo -e "Generating DARK GREEN tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(0,168,0)" "(0,41,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(0,168,0)" "(0,41,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(0,168,0)" $TIP_TINT_STRENGTH
 merge_images dark_green
 
 # dark aqua
 echo -e "Generating DARK AQUA tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(0,168,168)" "(0,41,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(0,168,168)" "(0,41,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(0,168,168)" $TIP_TINT_STRENGTH
 merge_images dark_aqua
 
 # dark red
 echo -e "Generating DARK RED tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(168,0,0)" "(41,0,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(168,0,0)" "(41,0,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(168,0,0)" $TIP_TINT_STRENGTH
 merge_images dark_red
 
 # dark purple
 echo -e "Generating DARK PURPLE tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(168,0,168)" "(41,0,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(168,0,168)" "(41,0,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(168,0,168)" $TIP_TINT_STRENGTH
 merge_images dark_purple
 
 # gold
 echo -e "Generating GOLD tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(252,168,0)" "(62,41,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(252,168,0)" "(62,41,0)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(252,168,0)" $TIP_TINT_STRENGTH
 merge_images gold
 
 # gray
 echo -e "Generating GRAY tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(168,168,168)" "(41,41,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(168,168,168)" "(41,41,41)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(168,168,168)" $TIP_TINT_STRENGTH
 merge_images gray
 
 # dark gray
 echo -e "Generating DARK GRAY tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(84,84,84)" "(21,21,21)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(84,84,84)" "(21,21,21)" $HIGH_FACTOR_DARK $LOW_FACTOR_DARK
 python3 image_color_multiply.py tipped_arrow/tip.png "(84,84,84)" $TIP_TINT_STRENGTH
 merge_images dark_gray
 
 # blue
 echo -e "Generating BLUE tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(84,84,252)" "(21,21,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(84,84,252)" "(21,21,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(84,84,252)" $TIP_TINT_STRENGTH
 merge_images blue
 
 # green
 echo -e "Generating GREEN tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(84,252,84)" "(21,62,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(84,252,84)" "(21,62,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(84,252,84)" $TIP_TINT_STRENGTH
 merge_images green
 
 # aqua
 echo -e "Generating AQUA tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(84,252,252)" "(21,62,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(84,252,252)" "(21,62,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(84,252,252)" $TIP_TINT_STRENGTH
 merge_images aqua
 
 # red
 echo -e "Generating RED tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(251,84,84)" "(62,21,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(251,84,84)" "(62,21,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(251,84,84)" $TIP_TINT_STRENGTH
 merge_images red
 
 # light purple
 echo -e "Generating LIGHT PURPLE tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(252,84,252)" "(62,21,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(252,84,252)" "(62,21,62)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(252,84,252)" $TIP_TINT_STRENGTH
 merge_images light_purple
 
 # yellow
 echo -e "Generating YELLOW tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(252,252,84)" "(62,62,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(252,252,84)" "(62,62,21)" $HIGH_FACTOR_MID $LOW_FACTOR_MID
 python3 image_color_multiply.py tipped_arrow/tip.png "(252,252,84)" $TIP_TINT_STRENGTH
 merge_images yellow
 
 # white
 echo -e "Generating WHITE tipped arrow image..."
-python3 image_color_replace_by_value_linear.py tipped_arrow/tail.png "(252,252,252)" "(62,62,62)" $HIGH_FACTOR_LIGHT $LOW_FACTOR_LIGHT
+python3 image_color_replace_by_value.py tipped_arrow/tail.png "(252,252,252)" "(62,62,62)" $HIGH_FACTOR_LIGHT $LOW_FACTOR_LIGHT
 python3 image_color_multiply.py tipped_arrow/tip.png "(252,252,252)" $TIP_TINT_STRENGTH
 merge_images white
